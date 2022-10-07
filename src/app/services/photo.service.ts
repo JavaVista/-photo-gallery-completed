77176f5915762a9fb7cca693831fd50ee4947eed
Javier 'Javi' Carrion <javier.carrion@designinteractive.net>
2022-10-07 17:32:04 -0400

    feat(android-native): :sparkles: add android native permissions & delete action sheet
    Added permissions to the Android native device to access device storage. I added a delete action sheet with a called to delete method in the photo.service

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -90,6 +90,25 @@ export class PhotoService {
     }
   }
 
+  async deletePicture(photo: UserPhoto, position: number) {
+    // Remove this photo from the Photos reference data array
+    this.photos.splice(position, 1);
+    // Update photos array cache by overwriting the existing photo array
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+    // delete photo file from filesystem
+    const filename = photo.filepath
+                        .slice(photo.filepath.lastIndexOf('/') + 1);
+  
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: Directory.Data
+    });
+  }
+
+
   private async readAsBase64(photo: Photo) {
     // "hybrid" will detect Cordova or Capacitor
     if (this.platform.is('hybrid')) {
